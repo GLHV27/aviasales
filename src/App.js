a23b25b1df8079a5e889a376539bca1fad6a66bd
Evgeni <glukhov91@mail.ru>
2018-07-16 12:30:36 +0300

    first commit

---
+++ src/App.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+
+import './statics/style/index.less';
+
+class App extends React.Component {
+
+    render() {
+        return (
+            <div className="">
+            </div>
+        )
+    }
+};
+
+export default App;
